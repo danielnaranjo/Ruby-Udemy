bc0bbc826327e8ffbe6591048923e6fd711db56b
Daniel Naranjo <dnaranjo@gmail.com>
2013-09-06 15:35:07 -0430

    Case Statement

---
+++ case.rb
@@ -0,0 +1,26 @@
+# case exression
+#   when expression1
+#      statements
+#   when expression2
+#      statements  
+#   when expression3
+#      statements
+#   else
+#      statements
+# end
+
+print("Enter a grade: ")
+grade = Integer(gets)
+case grade
+   when 90..100
+	letterGrade = "A"
+   when 80..89
+	letterGrade = "B"
+   when 70..79
+	letterGrade = "C"
+   when 60..69
+	letterGrade = "D"
+   else
+	letterGrade = "F"
+end
+print(letterGrade)
