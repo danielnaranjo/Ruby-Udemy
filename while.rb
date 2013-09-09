84587dc5a356db8a5964d035476f0bfadbba7c24
Daniel Naranjo <dnaranjo@gmail.com>
2013-09-09 18:23:50 -0430

    While vs Until
    Good comparison

---
+++ while.rb
@@ -0,0 +1,17 @@
+# while comparison
+# 	statements
+# end
+
+#count = 1
+#while (count <= 5)
+#	puts("Hello World")
+#	count +=1
+#end
+
+sum = 0
+number = 1
+while number <= 10
+	sum += number
+	number+=1
+end
+puts(sum)
