62eff1b6867e82b8809a67807c6723d213d119e5
Daniel Naranjo <dnaranjo@gmail.com>
2013-09-09 18:48:01 -0430

    Using Next to prevent error

--- pre.rb
+++ pre.rb
@@ -4,7 +4,11 @@ while (cont == 'y')
 	num = Integer(gets)
 	print("Enter a denominator: ")
 	denom = Integer(gets)
-	
+
+	if denom == 0 then
+		next
+	end
+
 	puts(num/denom)
 	print("More? y/n")
 	cont = gets
